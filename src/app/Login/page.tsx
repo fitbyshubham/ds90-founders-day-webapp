452aae425e096b96d992aae854deff4dc6c5528a
[Samarth Pundeer] <[samarth.pundeer@gmail.com]>
2025-09-15 15:52:54 +0530

    Login Page Updated UI

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,106 +1,50 @@
 "use client"
-import { useState } from "react";
-import './login.css'
-import { useRouter } from "next/navigation";
 
-export default function Login(){
-  const [isSignup, setIsSignup] = useState(false);
-  const router = useRouter()
-  const handleRedirect = (e: React.FormEvent) => {
-    e.preventDefault()
-    router.push("/OTP")
-  }
+import React, { useState } from 'react';
+import { Lock, Phone } from 'lucide-react';
 
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-beige-50">
-      <div className="relative flex flex-col items-center">
-        {/* Toggle */}
-        <label className="relative flex items-center justify-center gap-6 w-12 h-5 mb-8 cursor-pointer">
+export default function LoginPage() {
+  const [showPassword, setShowPassword] = useState(false);
+  
+  return(
+    <div className="min-h-screen flex items-center justify-center bg-neutral-900 px-4">
+      <div className="w-full max-w-sm bg-neutral-900 text-white">
+
+          <h2 className="text-2xl font-semibold mb-1">Log in to your account ✨</h2>
+          <p className="text-sm text-neutral-400 mb-6">
+            Welcome back! Please enter your details.
+          </p>
+
+          <label className="block text-sm mb-2">Mobile Number</label>
+          <div>
           <input
-            type="checkbox"
-            className="sr-only peer"
-            checked={isSignup}
-            onChange={() => setIsSignup(!isSignup)}
+            type="tel"
+            autoComplete="tel"
+            pattern="[0-9]{10}"
+            maxLength={10}
+            placeholder="Enter your mobile number"
+            className="w-full p-3 mb-4 rounded-lg bg-neutral-800 text-sm focus:outline-none focus:ring-2 focus:ring-pink-500"
+            required
           />
-
-          {/* Log in text */}
-          <span
-            className={`absolute -left-20 ${
-              !isSignup ? "font-bold text-black" : "font-semibold text-gray-800"
-            }`}
-          >
-            Log in
-          </span>
-
-          {/* Sign up text */}
-          <span
-            className={`absolute left-20 ${
-              isSignup ? "font-bold text-black" : "font-semibold text-gray-800"
-            }`}
-          >
-            Sign up
-          </span>
-
-          {/* Slider bar */}
-          <span className="w-full h-full bg-white border-2 border-gray-800 rounded peer-checked:bg-blue-500 transition"></span>
-
-          {/* Knob */}
-          <span className="absolute h-5 w-5 bg-white border-2 border-gray-800 rounded shadow-[3px_3px_0px_black] transition-transform peer-checked:translate-x-7"></span>
-        </label>
-
-        {/* Flip Card */}
-        <div className="relative w-[300px] h-[350px] [perspective:1000px]">
-          <div
-            className={`relative w-full h-full transition-transform duration-700 [transform-style:preserve-3d] ${
-              isSignup ? "[transform:rotateY(180deg)]" : ""
-            }`}
-          >
-            {/* Front (Login) */}
-            <div className="absolute w-full h-full flex flex-col justify-center items-center gap-5 bg-beige-200 border-3 border-brown-800 rounded shadow-[4px_4px_0px_black] [backface-visibility:hidden]">
-              <h2 className="text-2xl font-extrabold text-brown-800">Log in</h2>
-              <form className="flex flex-col items-center gap-4">
-                <input
-                  type="email"
-                  placeholder="Email"
-                  className="w-60 h-10 px-3 border-3 bg-beige-100 border-brown-800 rounded shadow-[4px_4px_0px_black] font-semibold text-gray-800 placeholder-gray-500 focus:border-amber-100 outline-none"
-                />
-                <input
-                  type="password"
-                  placeholder="Password"
-                  className="w-60 h-10 px-3 border-3 bg-beige-100 border-brown-800 rounded shadow-[4px_4px_0px_black] font-semibold text-gray-800 placeholder-gray-500 focus:border-amber-100 outline-none"
-                />
-                <button className="w-32 h-10 border-3 border-brown-800 rounded shadow-[4px_4px_0px_black] font-semibold text-brown-800 hover:translate-x-[2px] hover:translate-y-[2px] hover:shadow-none transition">
-                  Let`s go!
-                </button>
-              </form>
-            </div>
-
-            {/* Back (Signup) */}
-            <div className="absolute w-full h-full flex flex-col justify-center items-center gap-5 bg-beige-200 border-3 border-brown-800 rounded shadow-[4px_4px_0px_black] [transform:rotateY(180deg)] [backface-visibility:hidden]">
-              <h2 className="text-2xl font-extrabold text-brown-800">Sign up</h2>
-              <form 
-              onSubmit={handleRedirect}
-              className="flex flex-col items-center gap-4">
-                <input
-                  type="text"
-                  placeholder="Name"
-                  className="w-60 h-10 px-3 border-3 bg-beige-100 border-brown-800 rounded shadow-[4px_4px_0px_black] font-semibold text-gray-800 placeholder-gray-500 focus:border-amber-100 outline-none"
-                />
-                <input
-                  type="tel"
-                  placeholder="Phone Number"
-                  className="w-60 h-10 px-3 border-3 bg-beige-100 border-brown-800 rounded shadow-[4px_4px_0px_black] font-semibold text-gray-800 placeholder-gray-500 focus:border-amber-100 outline-none"
-                />
-                <button 
-                type="submit"
-                className="w-32 h-10 border-3 border-brown-800 rounded shadow-[4px_4px_0px_black] font-semibold text-gray-800 hover:translate-x-[2px] hover:translate-y-[2px] hover:shadow-none transition">
-                  Confirm!
-                </button>
-              </form>
-            </div>
           </div>
+
+          <label className="block text-sm mb-2">Name</label>
+          <div className="relative mb-2">
+            <input
+              type="text"
+              placeholder="Name"
+              className="w-full p-3 rounded-lg bg-neutral-800 text-sm focus:outline-none focus:ring-2 focus:ring-pink-500"
+              required
+            />
+
+          <button 
+          
+          className="w-full bg-gradient-to-r from-pink-500 to-purple-500 py-3 mt-6 rounded-lg font-semibold"
+          >
+            Log In
+          </button>
         </div>
       </div>
     </div>
-  );
+  )
 }
